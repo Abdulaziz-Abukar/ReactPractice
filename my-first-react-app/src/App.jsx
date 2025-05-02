e5b8823062d99c4b3c8a3f44201e40a0ec9d43ca
Abdulaziz Abukar <aabuk19@wgu.edu>
2025-05-02 13:37:49 -0600

    adds a new component called SimpleMultiplication that renders the product of two numbers. also adds styling to index.html through index.css

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import BusinessCard from "./components/BusinessCard/BusinessCard";
 import FactsAboutMe from "./components/Facts-About-Me-List/Facts-About-Me-List";
+import SimpleMultiplcation from "./components/SimpleMulitiplication/SimpleMultiplication";
 
 function App() {
   return (
     <>
       <BusinessCard />
       <FactsAboutMe />
+      <SimpleMultiplcation />
     </>
   )
 }
---
+++ my-first-react-app/src/components/SimpleMulitiplication/SimpleMultiplication.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './SimpleMulitiplication.css';
+
+function SimpleMultiplcation() {
+    const a = 10;
+    const b = 10;
+
+    return <p className="multiplication">{a * b}</p>
+}
+
+export default SimpleMultiplcation
