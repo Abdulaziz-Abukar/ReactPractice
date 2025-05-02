98a3f118348df463aa6ae1f83324dd2b3e08a050
Abdulaziz Abukar <aabuk19@wgu.edu>
2025-05-02 13:25:03 -0600

    renders BusinessCard and Facts-About-Me-List components to App.jsx

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import BusinessCard from "./components/BusinessCard/BusinessCard";
+import FactsAboutMe from "./components/Facts-About-Me-List/Facts-About-Me-List";
 
 function App() {
-  return <BusinessCard />
+  return (
+    <>
+      <BusinessCard />
+      <FactsAboutMe />
+    </>
+  )
 }
 
 export default App;
