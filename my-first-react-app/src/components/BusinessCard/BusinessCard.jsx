d7ed233bc396492366a9167f1971f55d47e474f7
Abdulaziz Abukar <aabuk19@wgu.edu>
2025-05-02 03:18:52 -0600

    creates BusinessCard jsx file and css. also gets rid of templates for react creation

---
+++ my-first-react-app/src/components/BusinessCard/BusinessCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import AbdulImage from '../../assets/Abdul.jpg';
+import './BusinessCard.css';
+
+
+function BusinessCard() {
+    const abdul = {
+        name: 'Abdulaziz Abukar',
+        'job title': 'Software Developer',
+        location: 'Calgary, AB',
+        contact: 'https://www.linkedin.com/in/abdul-abukar/',
+        src: AbdulImage,
+        alt: 'Image of me'
+    };
+
+    return (
+     <div className="business-card">
+        <h1>{abdul.name}</h1>
+        <img src={abdul.src} alt={abdul.alt} />
+        <h2>{abdul['job title']}</h2>
+        <p>{abdul.location}</p>
+        <p><a href={abdul.contact} target="_blank">LinkedIn</a></p>
+     </div>   
+    );
+}
+
+export default BusinessCard;
