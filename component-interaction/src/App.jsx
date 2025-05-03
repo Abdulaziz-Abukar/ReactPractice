6f8cd7746b53cfc6656c3125d6c3d6f7a1f6ab85
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2025-05-03 00:21:41 -0600

    creates a ParentControlsChildren.jsx and css file and updates App.jsx so it uses useState to create a counter

--- component-interaction/src/App.jsx
+++ component-interaction/src/App.jsx
@@ -2,14 +2,24 @@ import React from "react";
 import CustomButton from "./Components/CustomButton/CustomButton";
 import MessageCard from "./Components/MessageCard/MessageCard";
 import ToggleSwitch from "./Components/ToggleSwitch/ToggleSwitch";
-
+import ParentControlsChildren from "./Components/ParentControlsChildren/ParentControlsChildren";
+import { useState } from "react";
 function App() {
+  const [counter, setCounter] = useState(0);
   function alertMessage() {
-      return alert('This button has been clicked!');
+    return alert('This button has been clicked!');
   }
 
   function onToggle() {
     return alert('Toggled!')
+  }
+
+  function handleClick() {
+    setCounter(counter + 1);
+  }
+
+  function DisplayCount(props) {
+    return <p>The count is: {props.count}</p>
   }
   
     return (
@@ -20,6 +30,10 @@ function App() {
           <p>This is inside the card!</p>
         </MessageCard>
         <ToggleSwitch isOn={true} onToggle={onToggle} />
+        <ParentControlsChildren>
+          <button onClick={handleClick}>Click Me!</button>
+          <DisplayCount count={counter} />
+        </ParentControlsChildren>
       </>
   );
 }
---
+++ component-interaction/src/Components/ParentControlsChildren/ParentControlsChildren.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ParentControlsChildren(props) {
+
+    return (
+        <>
+            {props.children}
+        </>
+    )
+}
+
+export default ParentControlsChildren
