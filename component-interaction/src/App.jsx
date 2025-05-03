15f0a6062c98dbdc1a9c2fd70df63110c5156d5d
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2025-05-03 00:01:24 -0600

    adds ToggleSwitch component

--- component-interaction/src/App.jsx
+++ component-interaction/src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import CustomButton from "./Components/CustomButton/CustomButton";
 import MessageCard from "./Components/MessageCard/MessageCard";
+import ToggleSwitch from "./Components/ToggleSwitch/ToggleSwitch";
 
 function App() {
-    function alertMessage() {
-        return alert('This button has been clicked!');
-    }
+  function alertMessage() {
+      return alert('This button has been clicked!');
+  }
+
+  function onToggle() {
+    return alert('Toggled!')
+  }
+  
     return (
       <>
         <CustomButton label="Button" onClick={alertMessage} />
@@ -13,6 +19,7 @@ function App() {
           <h2>Hello Abdulaziz</h2>
           <p>This is inside the card!</p>
         </MessageCard>
+        <ToggleSwitch isOn={true} onToggle={onToggle} />
       </>
   );
 }
