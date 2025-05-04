91479ea1a4b43f09710e2f51a48b00a5a7555984
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2025-05-04 14:23:29 -0600

    creates 2 folders an easy folder for past components and a new medium folder for newer componenets for harder tasks. this new folder contains a mini shopping cart component that has a search bar for filtering items and an add and remove feature, also displaying the items added to cart

--- practicing-use-states/src/App.jsx
+++ practicing-use-states/src/App.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import BasicCounter from "./components/BasicCounter/BasicCounter";
-import ToggleText from "./components/ToggleText/ToggleText";
-import InputTracker from "./components/InputTracker/InputTracker";
-import SecretMessage from "./components/SecretMessage/SecretMessage";
-import { CountdownPlusReset } from "./components/CountdownPlusReset/CountdownPlusReset";
+import BasicCounter from "./components/Easy/BasicCounter/BasicCounter";
+import ToggleText from "./components/Easy/ToggleText/ToggleText";
+import InputTracker from "./components/Easy/InputTracker/InputTracker";
+import SecretMessage from "./components/Easy/SecretMessage/SecretMessage";
+import { CountdownPlusReset } from "./components/Easy/CountdownPlusReset/CountdownPlusReset";
+import { MiniShoppingCart } from "./components/Medium/MiniShoppingCart";
 
 function App() {
     return (
         <>
-            <BasicCounter label='Click Me!'/>
+            {/* <BasicCounter label='Click Me!'/>
             <ToggleText label='Toggle'/>
             <InputTracker />
             <SecretMessage visibleButton='Show Secret' invisibleButton='Hide Secret'/>
-            <CountdownPlusReset countDown="Click to decrement!" reset="Reset"/> 
+            <CountdownPlusReset countDown="Click to decrement!" reset="Reset"/>  */}
+            <MiniShoppingCart />
         </>
     )
 }
