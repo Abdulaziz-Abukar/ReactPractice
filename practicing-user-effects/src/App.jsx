71652e5ed5f778d0280e3f4cbda503cad5157a3c
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2025-05-06 01:07:19 -0600

    creates a window width and height tracker component that tracks the users width and height resolutions and displays them on the screen using usestates, useeffects and custom event listeners.

--- practicing-user-effects/src/App.jsx
+++ practicing-user-effects/src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { ComponentLogger } from "./components/ComponentLogger/ComponentLogger";
 import { TypingTracker } from "./components/TypingTracker/TypingTracker";
 import { TimerWithCleanup } from "./components/TimerWithCleanup/TImerWithCleanup";
+import { WinWidthAndHeightTracker } from "./components/WindowWidthTracker/WinWidthAndHeightTracker";
 
 function App() {
   return (
     <>
       {/* <ComponentLogger /> */}
       {/* <TypingTracker /> */}
-      <TimerWithCleanup />
+      {/* <TimerWithCleanup /> */}
+      <WinWidthAndHeightTracker />
     </>
   );
 }
