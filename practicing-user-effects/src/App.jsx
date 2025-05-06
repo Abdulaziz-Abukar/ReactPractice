187b2904407829d5c1ddb5a70fd83bc54ac71e9c
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2025-05-05 21:49:22 -0600

    creates a new react folder thats meant for practicing with use effects. has two components: ComponentLogger that logs to the console based off input, and TypingTracker that tracks user inputs and displays accordingly. git push

---
+++ practicing-user-effects/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ComponentLogger } from "./components/ComponentLogger/ComponentLogger";
+import { TypingTracker } from "./components/TypingTracker/TypingTracker";
+
+function App() {
+  return (
+    <>
+      {/* <ComponentLogger /> */}
+      <TypingTracker />
+    </>
+  );
+}
+export default App;
