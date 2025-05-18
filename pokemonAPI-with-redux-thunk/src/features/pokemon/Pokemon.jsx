339ca245819b38837dc5e14ce08650f9ca439d60
Abdulaziz Abukar <abdulazizabubakarfaqih@gmail.com>
2025-05-17 18:30:30 -0600

    fixed typo error

--- pokemonAPI-with-redux-thunk/src/features/pokemon/Pokemon.jsx
+++ pokemonAPI-with-redux-thunk/src/features/pokemon/Pokemon.jsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchPokemon, fetchPokemonByName } from "./pokemonSlice";
 import { addToTeam } from "../team/teamSlice";
